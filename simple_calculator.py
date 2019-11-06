af387dbb0266410bde58b184ec77d01e27168087
Witold Prendota <wibson90@gmail.com>
2019-11-06 22:56:12 +0100

    New update regarding: - methods in simple calculator, - testing methods: addition and changes.
    Witold

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,37 +1,50 @@
 import argparse
+import logging
+import sys
 
 
 class SimpleCalculator:
 
-    def __init__(self, arg_parser=None):
-        self.parser = self.arg_parse(arg_parser)
+    def __init__(self, arg=None):
+        self.arg = arg
+        self.argp = self.create_arg_parse()
 
-        if self.parser.sum:
-            print(self.addition(self.parser.sum[0], self.parser.sum[1]))
+    def run(self):
+        self.check_arg(self.arg)
 
-        if self.parser.sub:
-            print(self.subtraction(self.parser.sub[0], self.parser.sub[1]))
+        try:
+            if self.argp.parse_args().sum:
+                print(self.addition(self.argp.parse_args().sum[0], self.argp.parse_args().sum[1]))
 
-        if self.parser.mult:
-            print(self.multiplication(self.parser.mult[0], self.parser.mult[1]))
+            if self.argp.parse_args().sub:
+                print(self.subtraction(self.argp.parse_args().sub[0], self.argp.parse_args().sub[1]))
 
-        if self.parser.div:
-            print(self.division(self.parser.div[0], self.parser.div[1]))
+            if self.argp.parse_args().mult:
+                print(self.multiplication(self.argp.parse_args().mult[0], self.argp.parse_args().mult[1]))
+
+            if self.argp.parse_args().div:
+                print(self.division(self.argp.parse_args().div[0], self.argp.parse_args().div[1]))
+        except Exception:
+            logging.error('Failed.', exc_info=True)
 
     @staticmethod
-    def arg_parse(arg_parser):
-        if arg_parser is None:
+    def create_arg_parse():
+        argp = argparse.ArgumentParser()
+        argp.add_argument('-sum', nargs=2, type=int, help="Add two values.")
+        argp.add_argument('-sub', nargs=2, type=int, help="Subtract two values.")
+        argp.add_argument('-mult', nargs=2, type=int, help="Multiply two values.")
+        argp.add_argument('-div', nargs=2, type=int, help="Divise two values.")
+
+        return argp
+
+    @staticmethod
+    def check_arg(arg):
+        if len(arg) > 1:
             print("Welcome to the simple calculator. Use '-h' argument to get the information about the commands.")
-
-            argp = argparse.ArgumentParser()
-            argp.add_argument('-sum', nargs=2, type=int, help="Add two values.")
-            argp.add_argument('-sub', nargs=2, type=int, help="Subtract two values.")
-            argp.add_argument('-mult', nargs=2, type=int, help="Multiply two values.")
-            argp.add_argument('-div', nargs=2, type=int, help="Divise two values.")
-
-            return argp.parse_args()
+            return True
         else:
-            return arg_parser
+            print("Please give an arguments. Use '-h' to list them.")
+            return False
 
     @staticmethod
     def addition(val_1, val_2):
@@ -51,4 +64,5 @@ class SimpleCalculator:
 
 
 if __name__ == "__main__":
-    SimpleCalculator()
+    sc = SimpleCalculator(sys.argv)
+    sc.run()
--- test_simple_calculator.py
+++ test_simple_calculator.py
@@ -1,34 +1,53 @@
 import unittest
 
-from types import SimpleNamespace
-from SimpleCalculator import SimpleCalculator
+from simple_calculator import SimpleCalculator
 
 
 class TestSimpleCalculator(unittest.TestCase):
-    def setUp(self):
-        self.sc = SimpleCalculator(SimpleNamespace(div=None, mult=None, sub=None, sum=None))
+    def test_constructor(self):
+        sc1 = SimpleCalculator(['simple_calculator.py', '-mult', '2', '2'])
+
+        self.assertEqual(sc1.arg[0], 'simple_calculator.py')
+        self.assertNotEqual(sc1.arg[0], '-mult')
+
+        print("test_constructor - Done")
+
+    def test_run(self):
+        sc1 = SimpleCalculator(['simple_calculator.py', '-div', '1'])
+        self.assertRaises(Exception, sc1.run())
+        print("test_run - Done")
 
     def test_arg_parse(self):
-        self.assertEqual(SimpleCalculator.arg_parse(self, SimpleNamespace(div=[2, 2], mult=None, sub=None, sum=None)),
-                         SimpleNamespace(div=[2, 2], mult=None, sub=None, sum=None))
-        self.assertEqual(SimpleCalculator.arg_parse(self, SimpleNamespace(div=None, mult=[2, 2], sub=None, sum=None)),
-                         SimpleNamespace(div=None, mult=[2, 2], sub=None, sum=None))
-        self.assertEqual(SimpleCalculator.arg_parse(self, SimpleNamespace(div=None, mult=None, sub=[2, 2], sum=None)),
-                         SimpleNamespace(div=None, mult=None, sub=[2, 2], sum=None))
-        self.assertEqual(SimpleCalculator.arg_parse(self, SimpleNamespace(div=None, mult=None, sub=None, sum=[2, 2])),
-                         SimpleNamespace(div=None, mult=None, sub=None, sum=[2, 2]))
+        self.assertTrue(SimpleCalculator.create_arg_parse())
+
+        print("test_arg_parse - Done")
+
+    def test_check_arg(self):
+        self.assertTrue(SimpleCalculator.check_arg(['simple_calculator.py', '-mult', '2', '2']))
+
+        self.assertFalse(SimpleCalculator.check_arg(['simple_calculator.py']))
+
+        print("test_check_arg - Done")
 
     def test_addition(self):
         self.assertEqual(SimpleCalculator.addition(2, 2), 4)
+        self.assertNotEqual(SimpleCalculator.addition(1, 5), 2)
+        print("test_addition - Done")
 
     def test_subtraction(self):
         self.assertEqual(SimpleCalculator.subtraction(2, 2), 0)
+        self.assertNotEqual(SimpleCalculator.subtraction(1, 5), 2)
+        print("test_subtraction - Done")
 
     def test_multiplication(self):
         self.assertEqual(SimpleCalculator.multiplication(2, 2), 4)
+        self.assertNotEqual(SimpleCalculator.multiplication(1, 5), 2)
+        print("test_multiplication - Done")
 
     def test_division(self):
         self.assertEqual(SimpleCalculator.division(2, 2), 1)
+        self.assertNotEqual(SimpleCalculator.division(1, 5), 2)
+        print("test_division - Done")
 
         with self.assertRaises(ZeroDivisionError):
             SimpleCalculator.division(2, 0)
